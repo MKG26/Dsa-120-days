6e62d2eafd8ee0ae42ca0cd5f4474c6a53759091
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-01-01 22:37:30 +0530

    Update string3.java

--- string3.java
+++ string3.java
@@ -1,3 +1,39 @@
+import java.util.*;
+import java.util.Scanner;
+
 public class string3 {
+
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your Email");
+
+        String a = sc.next();
+
+        String result = "";
+
+        for(int i=0;i<a.length();i++){
+
+            if(a.charAt(i)=='@'){
+                break;
+            }
+            else{
+                result += a.charAt(i);
+            }
+
+        }
+
+        System.out.println();
+        
+        System.out.println("Username is");
+
+        System.out.println(result);
+    
+    }
+
+    
+    
     
 }
