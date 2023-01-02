46d357016b0e8e7cbba7fd7ed9033412c5f2e092
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-01-02 16:56:41 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class array {
+
+    public static void main(String args[]){
+
+        int a[]=new int[5];
+
+        for(int i=0;i<5;i++){
+
+            a[i]=i;
+        System.out.println(a[i]);
+        }
+
+        
+    }
+    
+}
