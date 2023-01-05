49e2b272e364d1f49ea5275e46699396a4780c5c
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-01-05 17:52:18 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class bubblesort {
+
+    public static void printArray(int arr[]){
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+    
+    public static void main(String args[]){
+
+        int arr[]= {8,7,3,2,1};
+
+        for(int i=0;i<arr.length; i++){
+            for(int j=0; j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+
+                    int swap = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=swap;
+                }
+            }
+        }
+
+            printArray(arr);
+    }
+    
+}
