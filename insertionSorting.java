cdfa27c170eefb326e958286839b85000b91fba7
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-01-16 00:46:33 +0530

    Create insertionSorting.java

---
+++ insertionSorting.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class insertionSorting {
+    public static void main(String args[]){
+
+        int arr[] = {6,8,4,2,5,10,1};
+
+            int temp ;
+            
+            for(int i=1; i<7 ; i++ ){
+                temp = arr[i];
+                int j=i-1;
+
+                for(; j>=0;j--){
+
+                    if(arr[j]>temp){
+
+                        arr[j+1] = arr[j];
+                    }else{
+                        break;
+                    }
+                }
+                arr[j+1]= temp;
+            }
+
+            for(int i=0 ; i<7 ; i++){
+                System.out.print(arr[i]+" ");
+            }
+}
+    
+}
