b410c7a45f73257b6d5d9c1447110e1f1f5dac78
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-01-22 19:09:16 +0530

    Create mergeSort.java

---
+++ mergeSort.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class mergeSort {
+    public static void main(String args[]){
+    
+        int arr1[] = {1,3,5,7,8,9};
+
+        int arr2[] = {2,4,6};
+
+        int arr3[] = {0,0,0,0,0,0,0,0,0};
+
+
+        sort(arr1,6,arr2,3,arr3);
+
+
+    
+    }
+
+
+        public static void sort(int arr1[],int n,int arr2[],int m,int arr3[]){
+
+            int i=0;
+            int j=0;
+            int k=0;
+
+            while(i<n && j<m){
+                
+                if(arr1[i]<arr2[j]){
+                    arr3[k] = arr1[i];
+                    k++;
+                    i++;
+                }else{
+                    arr3[k]= arr2[j];
+                    k++;
+                    j++;
+                }
+            }
+
+            while(i<6){
+                arr3[k] = arr1[i];
+                k++;
+                i++;
+            }
+
+            while(j<3){
+                arr3[k]=arr2[j];
+                k++;
+                j++;
+            }
+
+            for(int h=0 ; h<arr3.length ; h++){
+                System.out.print(arr3[h]+ " ");
+            }
+            
+
+        } 
+}
