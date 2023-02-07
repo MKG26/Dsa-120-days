498f8c63db3c8468c61d0b2a81ba6d6197f85248
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-02-07 13:21:10 +0530

    Create class1.java

---
+++ class1.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class class1{
+      
+    public static void main(String args[]){
+    
+    
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int m= sc.nextInt();
+        int n= m;
+            int A[] = new int[n];
+
+            for(int h=0 ; h<m; h++){
+                A[h]= sc.nextInt();
+            }
+int j;
+int i;
+    
+            
+        for(i=0 ; i<n-1; i++){
+            
+            for(j=0; j<n-1-i ; j++){
+    
+                if(A[j]> A[j+1]){
+                    int t=A[j];
+                    A[j]= A[j+1];
+                    A[j+1]= t;
+                }               
+
+    
+    
+               
+    
+    }
+}
+
+for(int h=0 ; h<m; h++){
+    System.out.print(A[h]+" ");
+}
+
+    }
+    
+
+
+
+
+}
