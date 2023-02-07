7eabbe0e02ece4cd989a3589659b8acd199f96bf
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-02-07 19:07:03 +0530

    Create class2.java

---
+++ class2.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.util.Scanner;
+import java.time.Clock;
+
+
+public class class2 {
+    public static void main(String args[]){
+    
+        Scanner sc = new Scanner(System.in);
+        
+        int m= sc.nextInt();
+        int n= m;
+            int A[] = new int[n];
+
+            for(int h=0 ; h<m; h++){
+                A[h]= sc.nextInt();
+            }
+
+            Clock clock = Clock.systemUTC();
+
+
+int j;
+int i;
+    
+            
+        for(i=0 ; i<n-1; i++){
+            
+            for(j=0; j<n-1-i ; j++){
+    
+                if(A[j]> A[j+1]){
+                    int t=A[j];
+                    A[j]= A[j+1];
+                    A[j+1]= t;
+                }               
+
+    
+
+    
+               ;      
+    
+    }
+}
+
+System.out.println("t1 " + clock.instant());
+
+for(int h=0 ; h<m; h++){
+    System.out.print(A[h]+" ");
+}
+
+
+
+    }
+
+    
+    }
+
