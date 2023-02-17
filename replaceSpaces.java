a02dd458051ce41d693f17d08faa2fc835f7fcad
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-02-17 21:04:59 +0530

    Create replaceSpaces.java

---
+++ replaceSpaces.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java .util.Scanner;
+
+public class replaceSpaces {
+    
+    public static void main(String args[]){
+    
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+
+
+
+
+            
+               s=  s.replace(" ","@40");
+            
+        
+
+        System.out.println(s);
+    
+    }
+}
