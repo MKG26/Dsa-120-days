1e8a90968186c0116ccf3ff665d6a41d68fcace3
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-02-19 18:04:56 +0530

    Create dublicates.java

---
+++ dublicates.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.Scanner;
+
+
+public class dublicates {
+
+    public static void main(String args[]){
+    
+        Scanner sc = new Scanner(System.in);
+
+        String s = new String("abby");
+
+
+        String s2 = new String();
+
+
+        String sub = new String();
+
+        for(int i=0; i<s.length()-1; i++){
+            
+            char temp1 = s.charAt(i);
+
+            char temp2 = s.charAt(i+1);
+
+            
+            if(temp1 == temp2){
+                    sub = s.substring(0,i)+s.substring(i+2,s.length());
+
+            }
+
+            
+
+
+        }
+
+
+        System.out.println(sub);
+
+    
+    }
+    
+}
