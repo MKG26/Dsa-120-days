d304559e5f622e722906f41670caef4dc558674d
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-05-05 16:52:20 +0530

    Update _2d.java

--- _2d.java
+++ _2d.java
@@ -8,7 +8,9 @@ public class _2d {
         
         Scanner sc = new Scanner(System.in);
 
-            int arr[][] = new int[3][4];
+            int arr[][] ={{1,2,3,4},{5,6,7,8},{9,10,11,12}} ;
+
+        
 
             //row wise input
             // for(int i=0; i<3; i++){
@@ -17,21 +19,40 @@ public class _2d {
             //     }
             // }
 
-            //coloumn wise input
-            for(int i=0; i<4; i++){
-                for(int j=0; j<3; j++){
-                    arr[j][i]=sc.nextInt();
-                }
-            }
+            // //coloumn wise input
+            // for(int i=0; i<4; i++){
+            //     for(int j=0; j<3; j++){
+            //         arr[j][i]=sc.nextInt();
+            //     }
+            // }
 
 
-            for(int i=0; i<3; i++){
-                for(int j=0; j<4; j++){
-                    System.out.print(arr[i][j]+" ");
-                }
-                System.out.println();
-            }
+            // for(int i=0; i<3; i++){
+            //     for(int j=0; j<4; j++){
+            //         System.out.print(arr[i][j]+" ");
+            //     }
+            //     System.out.println();
+            // }
         
+                boolean ans =  isPresent(arr,45,3,4);
+
+                System.out.println(ans);
+
+            }        
     
+    
+
+        public static boolean isPresent(int arr[][],int target, int row, int col ){
+
+            for(int i=0; i<row; i++){
+                for(int j=0; j<col; j++){
+                    if(arr[i][j]==target){
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+
     }
 }
