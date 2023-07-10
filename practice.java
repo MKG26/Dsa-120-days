2892863a77a68e145d2b72919123e682bd6a9563
MKG26 <92150824+MKG26@users.noreply.github.com>
2023-07-10 13:38:16 +0530

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class practice {
+    public static void main(String args[]){
+    
+        int a = 2;
+
+        int b = a>>1;
+
+        System.out.println(b);
+    
+    }    
+}
