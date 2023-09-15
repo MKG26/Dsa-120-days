c19649aca1f6c33040b2c10505b1741f2f835583
Mohit <92150824+MKG26@users.noreply.github.com>
2023-09-15 23:18:16 +0530

    Create practice2.java

---
+++ practice2.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class practice2 {
+    
+    public static void main(String args[]){
+    
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[3][2];
+
+
+            for(int i=0 ; i<arr.length; i++){
+                for(int j=0; j<arr[i].length; j++){
+
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+             
+             
+             
+             for(int row=0; row<arr.length; row++){
+
+                System.out.println(Arrays.toString(arr[row]));
+             }
+    }
+}
