5d0aed3085d9ec06acff6aa9da7072437897a821
Mohit <92150824+MKG26@users.noreply.github.com>
2023-09-22 11:56:48 +0530

    Create binarySearchh.java

---
+++ binarySearchh.java
@@ -0,0 +1,41 @@
+
+
+public class binarySearchh {
+
+    public static void main(String args[]){
+    
+        int[] arr = {-2,-1,0,1,2,3,4,5,6,7,8,9};
+
+
+        System.out.println(search(arr,5));
+    
+    }
+
+    static int search(int[] arr, int target){
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<=end){
+
+            int mid = (start + ((end-start)/2));
+
+            if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else{
+                 return mid;
+            }
+
+
+        }
+
+        return -1;
+
+
+    }
+    
+}
