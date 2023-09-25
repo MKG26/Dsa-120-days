2fe5ceab53f4bff6be59e67d892e98c2c1bf160c
Mohit <92150824+MKG26@users.noreply.github.com>
2023-09-25 22:33:34 +0530

    Create check.java

---
+++ check.java
@@ -0,0 +1,16 @@
+public class check {
+    
+    public static void main(String args[]){
+    
+        int[] arr = {1,2};
+
+
+        if(arr[2] == 2){
+            System.out.println("t");
+        }
+        else{
+            System.out.println("f");
+        }
+    
+    }
+}
