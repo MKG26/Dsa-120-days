167f1f579f908bb16fce75e950bdaa4a1a6bb684
Mohit <92150824+MKG26@users.noreply.github.com>
2023-10-01 21:47:52 +0530

    Update lee.java

--- lee.java
+++ lee.java
@@ -6,15 +6,30 @@ public class lee {
     
         int[] arr = {1,2,3,4};
 
-        System.out.println(Arrays.toString(arr));
+
+        int[][] hh = new int[2][];
+
+
+        hh= form(arr);
+
+for(int r = 0; r<2 ; r++){
+            for(int c = 0; c<2; c++){
+
+                System.out.print(hh[r][c]);
+            }
+            System.out.println();
+        }
+        
+
+        
 
 
         
     }
 
-    static void form(){
+    static int[][] form(int[] arr ){
 
-        int[] arr = {1,2,3,4}; 
+
         
         int[][] arrr = new int[2][2];
         int temp = 0;
@@ -26,12 +41,14 @@ public class lee {
             }
         }
 
-            for(int i=0; i<2; i++){
-                for(int j =0; j<2;j++){
-                    System.out.print(arrr[i][j]+" ");
-                }
-                System.out.println();
-            }
+
+        return arrr;
+            // for(int i=0; i<2; i++){
+            //     for(int j =0; j<2;j++){
+            //         System.out.print(arrr[i][j]+" ");
+            //     }
+            //     System.out.println();
+            // }
     }
     
 }
