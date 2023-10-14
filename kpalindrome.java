af108902e9d77922af4a4f2cf5ea187a911055db
Mohit <92150824+MKG26@users.noreply.github.com>
2023-10-14 23:28:15 +0530

    Create kpalindrome.java

---
+++ kpalindrome.java
@@ -0,0 +1,32 @@
+public class kpalindrome {
+    public static void main(String args[]){
+    
+        String a = "madam";
+
+
+
+        System.out.println(check(a));
+    
+    }
+
+     static boolean check(String b){
+
+        int s = 0;
+
+        int e = b.length() -1 ;
+
+        while(s<e){
+
+            if(b.charAt(s) != b.charAt(e)){
+                return false;
+            }
+            else{
+                s++;
+                e--;
+            }
+        }
+
+        return true;
+    }   
+    
+}
