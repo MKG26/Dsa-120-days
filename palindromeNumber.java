608ef6f23e5445740a122ab9a786900f90438214
Mohit <92150824+MKG26@users.noreply.github.com>
2023-10-15 09:46:02 +0530

    Create palindromeNumber.java

---
+++ palindromeNumber.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class palindromeNumber {
+    
+    public static void main(String args[]){
+    
+        int a = 121;
+
+        String s = String.valueOf(a);
+    
+    }
+}
