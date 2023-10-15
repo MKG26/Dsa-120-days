47c3295e686ee2722f3096723df66936f163d8a2
Mohit <92150824+MKG26@users.noreply.github.com>
2023-10-15 23:24:38 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -2,84 +2,147 @@ public class pattern {
 
     public static void main(String args[]){
     
-        pattern5(5);
+        pattern8(5);
     
     }
 
-    static void pattern1(int n){
+    // static void pattern1(int n){
+
+    //     for(int row = 1; row<=n ; row++){
+
+    //         for(int col = 1; col<=row; col++){
+
+    //             System.out.print("* ");
+    //         }
+
+    //         System.out.println();
+    //     }
+    // }
+    
+    // static void pattern2(int n){
+
+    //     for(int row = 1; row<=n ; row++){
+
+    //         for(int col = 1; col<=5; col++){
+
+    //             System.out.print("*");
+    //         }
+
+    //         System.out.println();
+    //     }
+    // }
+
+    // static void pattern3(int n){
+
+    //     for(int row = 1; row<=n ; row++){
+
+    //         for(int col = 0; col<=n-row; col++){
+
+    //             System.out.print("* ");
+    //         }
+
+    //         System.out.println();
+    //     }
+    // }
+
+    // static void pattern4(int n){
+
+    //     for(int row = 1; row<=n ; row++){
+
+    //         for(int col = 1; col<=row; col++){
+
+    //             System.out.print(col + " ");
+    //         }
+
+    //         System.out.println();
+    //     }
+    // }
+
+
+    // }
+
+    // static void pattern5(int n){
+
+    //     for(int row = 1; row<=n ; row++){
+
+    //         for(int col = 1; col<=row; col++){
+
+    //             System.out.print("* ");
+    //         }
+
+    //         System.out.println();
+    //     }
+
+    //     for(int row = 1; row<n ; row++){
+
+    //         for(int col = 0; col<=n-row-1; col++){
+
+    //             System.out.print("* ");
+    //         }
+
+    //         System.out.println();
+    //     }
+    // }
+
+
+        static void pattern6(int n){
 
         for(int row = 1; row<=n ; row++){
 
-            for(int col = 1; col<=row; col++){
+            for(int col = n-row; col>0; col--){
 
-                System.out.print("* ");
+                System.out.print(" ");
             }
 
-            System.out.println();
-        }
-    }
-    
-    static void pattern2(int n){
-
-        for(int row = 1; row<=n ; row++){
-
-            for(int col = 1; col<=5; col++){
+            for(int ncol = row ; ncol >0; ncol--){
 
                 System.out.print("*");
             }
 
             System.out.println();
         }
+
     }
 
-    static void pattern3(int n){
+    //     static void pattern7(int n){
+
+    //     for(int row = 1; row<=n ; row++){
+
+    //         for(int col = 1; col<row; col++){
+
+    //             System.out.print(" ");
+    //         }
+
+    //         for(int ncol = n-row ; ncol >=0; ncol--){
+
+    //             System.out.print("*");
+    //         }
+
+    //         System.out.println();
+    //     }
+
+    // }
+
+
+        static void pattern8(int n){
 
         for(int row = 1; row<=n ; row++){
 
-            for(int col = 0; col<=n-row; col++){
+            for(int col = n-row; col>0; col--){
 
-                System.out.print("* ");
+                System.out.print(" ");
+            }
+
+            for(int ncol = row ; ncol >0; ncol--){
+
+                System.out.print("*");
             }
 
             System.out.println();
         }
-    }
-
-    static void pattern4(int n){
-
-        for(int row = 1; row<=n ; row++){
-
-            for(int col = 1; col<=row; col++){
-
-                System.out.print(col + " ");
-            }
-
-            System.out.println();
-        }
-
 
     }
 
-    static void pattern5(int n){
 
-        for(int row = 1; row<=n ; row++){
 
-            for(int col = 1; col<=row; col++){
-
-                System.out.print("* ");
-            }
-
-            System.out.println();
-        }
-
-        for(int row = 1; row<n ; row++){
-
-            for(int col = 0; col<=n-row-1; col++){
-
-                System.out.print("* ");
-            }
-
-            System.out.println();
-        }
-    }
-}
+ }
