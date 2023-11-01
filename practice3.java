9df78d25e104b18178bbc7a6633358f7dd3937a2
Mohit <92150824+MKG26@users.noreply.github.com>
2023-11-02 00:36:36 +0530

    Update practice3.java

--- practice3.java
+++ practice3.java
@@ -17,14 +17,44 @@ public class practice3 {
     
     static int longestConsecutive(int[] nums) {
         
-        int count = 0;
+        int ans = 0;
         
         for(int i=0; i<nums.length; i++){
 
+           int j = 0; 
+
+           int count = 0;
+
+           
+
+            int value = nums[i];
+
+            while(true){
+
+               
+
+                if(search(nums,value )){
+                    count++;
+                    
+                    value++;
+                }
+                else{
+                    break;
+                }
+
+            }
+
+            if(ans<count){
+                ans = count;
+            }
+
+         
             
 
 
         }
+
+        return ans;
 
    
 }
