3b03ee1490228eb7a8798d79b3a7f39b60339f16
Mohit <92150824+MKG26@users.noreply.github.com>
2023-11-20 23:39:50 +0530

    Update practice4.java

--- practice4.java
+++ practice4.java
@@ -2,9 +2,11 @@ public class practice4 {
     
     public static void main(String args[]){
     
-        double a = 7788/10;
+        float a = 7788/10;
 
         System.out.println(a);
+
+            
     
     }   
 }
