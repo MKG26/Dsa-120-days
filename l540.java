206167b40a3c480554fe4a194ad5f425b57653af
Mohit <92150824+MKG26@users.noreply.github.com>
2023-12-06 17:04:14 +0530

    Update l540.java

--- l540.java
+++ l540.java
@@ -9,34 +9,33 @@ public class l540 {
 
     static int singleNonDuplicate(int[] nums) {
         
-        for(int i=1; i<nums.length; i++){
+        for(int i=0; i<nums.length; i++){
 
-            if(i == nums.length-1 && nums[i] != nums[i-1]
- ){
-                return nums[i];
+
+            if(nums.length == 1){
+                return nums[0];
             }
-            
-            if(i<nums.length-1)
-             {if(nums[i] == nums[i+1] || nums[i-1] == nums[i]){
-                
+
+            if(i == 0){
+                if(nums[i] != nums[i+1]){
+                    return nums[i];
+                }
             }
-            else{
-                return nums[i];
-            }}
-          
-        }
+            else if(i == nums.length-1){
 
-        
-        
-        if(nums.length ==1){
-            return nums[0];
-        }
-
-        if(nums[0] != nums[1]){
-            return nums[0];
-        }
-
-        return -1;
+                if(nums[i] != nums[i-1]){
+                    return nums[i];
+                }
+            }
+            else
+            {
+                if(nums[i] != nums[i-1] && nums[i] != nums[i+1]){
+                    return nums[i];
+                }
+            }
         
     }
+
+    return -1;
+}
 }
