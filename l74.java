e660d059a39aa0eae28ec7dd881a84ffcdb63dcc
Mohit <92150824+MKG26@users.noreply.github.com>
2023-12-15 01:55:13 +0530

    .

--- l74.java
+++ l74.java
@@ -4,7 +4,7 @@ public class l74 {
 
         int[][] arr = {
                 {1,3,5,7},
-                {10,11,16,10},
+                {10,11,16,20},
                 {23,30,34,60}
         };
 
@@ -15,8 +15,44 @@ public class l74 {
 
         for(int i=0; i<matrix.length; i++){
 
+            if(matrix[i][0]<= target && matrix[i][matrix[0].length-1] >= target){
+
+                return bs(matrix, i, target);
+            }
 
         }
+
+        return false;
+
+    }
+
+    private static boolean bs(int[][] matrix, int i, int target) {
+
+        int start = 0;
+
+        int end = matrix[0].length-1;
+
+        while(start<=end){
+
+            int mid = start + (end-start)/2;
+
+            if(matrix[i][mid] < target){
+
+                start = mid+1;
+            }
+            else if(matrix[i][mid]> target){
+
+                end = mid-1;
+            }
+            else {
+
+                return true;
+            }
+
+        }
+
+        return false;
+
 
     }
 }
