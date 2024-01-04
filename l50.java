85e248c20a2f2e370d3327e86ad5082132414e31
Mohit <92150824+MKG26@users.noreply.github.com>
2024-01-05 00:27:30 +0530

    .

---
+++ l50.java
@@ -0,0 +1,14 @@
+public class l50 {
+
+    public static void main(String[] args) {
+
+        System.out.println(myPow(2,10));
+    }
+
+    public static double myPow(double x, int n) {
+
+        return (double) Math.pow(x,n);
+
+
+    }
+}
