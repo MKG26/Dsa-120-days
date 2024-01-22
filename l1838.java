f3c3a8657c55ac5d263f43e81aa733a49f67df06
Mohit <92150824+MKG26@users.noreply.github.com>
2024-01-23 00:17:53 +0530

    .

---
+++ l1838.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class l1838 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,4};
+
+        System.out.println(maxFrequency(nums, 5));
+    }
+
+    public static int maxFrequency(int[] nums, int k) {
+
+        int left =0;
+        int rigth = 0;
+
+        int ans = 0;
+        long sum = nums[0];
+
+        Arrays.sort(nums);
+
+        while(rigth < nums.length){
+
+            sum += nums[rigth];
+
+            while((long)nums[rigth]*(rigth-left+1) > sum + k){
+
+                sum -= nums[left];
+                left++;
+            }
+
+
+            ans = Math.max( ans, (rigth-left+1));
+            rigth++;
+        }
+
+
+        return ans;
+
+    }
+}
