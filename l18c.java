aa86f0c52730d34db04e6210b45f4d0eec9a6873
Mohit <92150824+MKG26@users.noreply.github.com>
2024-01-30 03:31:35 +0530

    .

---
+++ l18c.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class l18c {
+    public static void main(String[] args) {
+
+        System.out.println(fourSum(new int[]{1,0,-1,0,-2,2}, 0));
+
+    }
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i=0; i< n; i++){
+
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            for(int j=i+1; j< n ; j++){
+
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+
+                int k = j+1;
+                int l = n-1;
+
+                while(k<l){
+
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if(sum == target){
+
+                        List<Integer> temp = Arrays.asList(nums[i], nums[j] , nums[k], nums[l]);
+
+                        ans.add(temp);
+                        k++;
+                        l--;
+
+                        while(k<l && nums[k] == nums[k-1]) k++;
+                        while(k<l && nums[l] == nums[l+1]) l--;
+                    }
+                    else if(sum < target){
+
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
+
+                }
+            }
+        }
+
+        return ans;
+    }
+}
