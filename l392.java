a675a1e932db5da88e46568d6c71182f4da5d2e6
Mohit <92150824+MKG26@users.noreply.github.com>
2024-02-21 15:57:46 +0530

    .

---
+++ l392.java
@@ -0,0 +1,34 @@
+public class l392 {
+
+    public static void main(String[] args) {
+
+        System.out.println(isSubsequence("abc", "ahbgdc"));
+
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+
+
+        int i = 0;
+        int j =0 ;
+
+        while(i< s.length() && j < t.length()){
+
+            if(s.charAt(i) == t.charAt(j)){
+
+                i++;
+                j++;
+            }else {
+
+                j++;
+            }
+        }
+
+        if(i == s.length()){
+            return true;
+        }
+
+
+        return false;
+    }
+}
